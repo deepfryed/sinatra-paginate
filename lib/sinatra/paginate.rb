8ddffd64d276d69a663aca29d8707ec18e300c11
nika.miller <nika.miller@rackspace.com>
2014-02-12 22:29:35 -0700

    changed hashes back to 1.8.7 style, created separate view files, added offset value

--- lib/sinatra/paginate.rb
+++ lib/sinatra/paginate.rb
@@ -4,21 +4,21 @@ require 'uri'
 
 module Sinatra
   module Paginate
-    DEFAULTS = {items_per_page: 10, width: 5, renderer: 'haml', labels: {first: '«', last: '»'}}
+    DEFAULTS = {:items_per_page => 20, :width => 5, :renderer => 'erubis', :labels => {:first => '«', :last => '»'}}
 
     def paginate resource, options = {}
       raise ArgumentError, 'resource should respond to :total' unless resource.respond_to?(:total)
       raise ArgumentError, 'resource should respond to :size'  unless resource.respond_to?(:size)
 
       options  = DEFAULTS.merge(options)
-      view     = options.fetch(:view, paginate_haml)
+      view     = options.fetch(:view, paginate_erb)
       renderer = options.fetch(:renderer)
-      send(renderer, view, layout: false, locals: paginate_options(resource, options))
+      send(renderer, view, :layout => false, :locals => paginate_options(resource, options))
     end
 
     def paginate_options resource, options
       last = (resource.total / options[:items_per_page].to_f).ceil
-      page = [1, params['page'].to_i].max
+      page = [1, params[:page].to_i].max
       from = [1, page - (options[:width] >> 1)].max
       to   = [from + options[:width] - 1, last].min
 
@@ -26,16 +26,31 @@ module Sinatra
         to   += 1 if to   < last
         from -= 1 if from > 1
       end
-      {page: page, from: from, to: to, last: last, uri: options.fetch(:uri, request.path_info), labels: options.fetch(:labels)}
+
+      {:page => page, :from => from, :to => to, :last => last, :uri => options.fetch(:uri, request.path_info), :labels => options.fetch(:labels)}
     end
 
     def paginate_haml
-      @@haml ||= File.read(__FILE__).sub %r{^.*__END__}m, ''
+      @@haml ||= File.read(File.join(File.dirname(__FILE__), 'views', 'pagination_nav.haml'))
+    end
+
+    def paginate_erb
+      @@erubis ||= File.read(File.join(File.dirname(__FILE__), 'views', 'pagination_nav.erb'))
     end
 
     def page
-      page = params['page'].to_i
+      page = params[:page].to_i
       page > 0 ? page : 1
+    end
+
+    def offset_value items_per_page
+      (page * items_per_page) - items_per_page
+    end
+
+    def viewing_text offset, total, items_per_page
+      from_this = total == 0 ? offset : offset + 1
+      to_that = (offset + items_per_page) > total ? total : (offset + items_per_page)
+      "#{from_this} - #{to_that} of #{total}"
     end
 
     def pagination_url *path
@@ -51,22 +66,3 @@ module Sinatra
 
   register Paginate
 end # Sinatra
-
-__END__
-:css
-  ul.sinatra-pagination > li {
-    float: left;
-    margin-right: 10px;
-    display: inline-block;
-    list-style-type: none;
-  }
-
-- pagination_css = {-1 => 'btn-prev', 1 => 'btn-next', 0 => 'btn-primary'}
-%ul.sinatra-pagination
-  %li
-    %a.btn.btn-first{href: pagination_url(uri, params.merge('page' => 1))}= labels[:first]
-  - (from..to).each do |n|
-    %li
-      %a{href: pagination_url(uri, params.merge('page' => n)), class: "btn #{pagination_css[n - page]}"}= n
-  %li
-    %a.btn.btn-last{href: pagination_url(uri, params.merge('page' => last))}= labels[:last]
--- sinatra-paginate.gemspec
+++ sinatra-paginate.gemspec
@@ -2,18 +2,18 @@
 
 Gem::Specification.new do |s|
   s.name = "sinatra-paginate"
-  s.version = "0.1.0"
+  s.version = "0.1.1"
 
   s.required_rubygems_version = Gem::Requirement.new(">= 0") if s.respond_to? :required_rubygems_version=
-  s.authors = ["Bharanee Rathna"]
-  s.date = "2012-06-18"
-  s.description = "Simple Sinatra pagination helper."
-  s.email = ["deepfryed@gmail.com"]
-  s.files = ["test/helper.rb", "test/test_haml.rb", "lib/sinatra/paginate.rb", "README.md", "Gemfile", "Gemfile.lock", "CHANGELOG"]
-  s.homepage = "http://github.com/deepfryed/sinatra-paginate"
+  s.authors = ["Bharanee Rathna", "Nika Miller"]
+  s.date = "2014-02-12"
+  s.description = "Simple Sinatra pagination helper, modified to work with Ruby 1.8.7 and erb."
+  s.email = ["miller.nika@gmail.com"]
+  s.files = ["test/helper.rb", "test/test_haml.rb", "lib/sinatra/paginate.rb", "lib/sinatra/views/pagination_nav.erb", "lib/sinatra/views/pagination_nav.haml", "README.md", "Gemfile", "Gemfile.lock", "CHANGELOG"]
+  s.homepage = "http://github.com/nika-m/sinatra-paginate"
   s.require_paths = ["lib"]
   s.rubygems_version = "1.8.24"
-  s.summary = "Sinatra pagination helper."
+  s.summary = "Sinatra Pagination Helper."
 
   if s.respond_to? :specification_version then
     s.specification_version = 3
